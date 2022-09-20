7adf70c3a428edcffaded4ab2869285276966d6e
yeojinseo <113421483+yeojinseo@users.noreply.github.com>
2022-09-20 17:33:32 +0900

    04

--- main.c
+++ main.c
@@ -4,12 +4,15 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	char text;
+	int a, b;
 	
-	printf("input a character : ");
-	scanf("%c", &text);
+	printf("분자를 입력하세요 : ");
+	scanf("%d", &a);
 	
-	printf("the next character of %c (%d) is %c (%d)\n", text, text, text+1, text+1);
+	printf("분모를 입력하세요 : ");
+	scanf("%d", &b);
+
+	printf("나누기의 결과는 %f 입니다.", (float)a/b);
 	
 	return 0;
 }
